62d4aad0137801bc1350e59d301d60ed0113806a
Noah T <noahtofte@gmail.com>
2023-06-03 11:33:52 -0400

    commit before adding answer modal

--- client/src/components/QuestionList.tsx
+++ client/src/components/QuestionList.tsx
@@ -1,22 +1,34 @@
 import './QuestionList.css';
+import styles from '../assets/styles/QuestionList.module.scss';
 
 export default function QuestionList({ loading, questions, onRequestUpdate }) {
   return (
-    <div className='questions'>
-      <h1 className='questions-header'>Interview Questions</h1>
+    <div className={styles.questionListContainer}>
+      <h1 className={styles.questionsHeader}>Interview Questions</h1>
       {loading ? (
         <div className='loading'>Loading...</div>
       ) : (
-        <ul className='question-list'>
+        // <ul className={styles.questionList}>
+        //   {questions.map((question, i) => {
+        //     return (
+        //       <li key={i} className={styles.questionItem}>
+        //         <h3 className='question-title'>{question.title}</h3>
+        //         <p className='question-text'>{question.question}</p>
+        //       </li>
+        //     );
+        //   })}
+        // </ul>
+        <div className={styles.questionList}>
           {questions.map((question, i) => {
             return (
-              <li key={i} className='question-item'>
+              <div key={i} className={styles.questionItem}>
                 <h3 className='question-title'>{question.title}</h3>
                 <p className='question-text'>{question.question}</p>
-              </li>
+                <button>Add Answer</button>
+              </div>
             );
           })}
-        </ul>
+        </div>
       )}
     </div>
   );
